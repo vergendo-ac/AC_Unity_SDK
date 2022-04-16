a8b4c83596b7153f3f24cd42e2f84f762d9f9a36
Sergey Fedorenko <serigfed@gmail.com>
2022-04-17 02:41:15 +0300

    Max AR-camera level above the ground is set to 1.6m

--- ACViewer-SDK/Assets/_Scripts/AR/PlaneManager.cs
+++ ACViewer-SDK/Assets/_Scripts/AR/PlaneManager.cs
@@ -10,6 +10,10 @@ public class PlaneManager : MonoBehaviour
     int planetimer = 0;
     List<GameObject> planes;
     GameObject aRcamera;
+    
+    const float yGroundMin = 0.8f;   // min AR-camera level above the ground, where ground is detected
+    const float yGroundDef = 1.5f;   // assume ground level which is under AR camera by 1.5m in case of not detected
+    const float yGroundMax = 1.6f;   // max AR-camera level above the ground, where ground is detected
 
     void Start()
     {
@@ -33,28 +37,31 @@ public class PlaneManager : MonoBehaviour
         //Debug.Log("planes length = " + planes.Count);
 
         float minY = 10;
-        foreach (GameObject pl in planes) {
-            // search for the plane on distance >0.8m from the camera
-            if (aRcamera.transform.position.y - pl.transform.position.y > 0.8f) {
+        foreach (GameObject pl in planes)
+        {
+            // search for the plane on distance >MIN from the camera
+            if (aRcamera.transform.position.y - pl.transform.position.y > yGroundMin)
+            {
                 //FixMe: How to proceed planes under the ground?
-                if (pl.transform.position.y < minY)
+                if (pl.transform.position.y < minY) {
                     minY = pl.transform.position.y;         // search for the minimal Y coord - closest plane to camera
+                }
             }
             planetimer = -50;                               // remeasure in 3sec if AR has found some planes
         }
 
         if (minY < 10)                                      // if found
         {
-            yGround = minY;                                 // store closest plane to the camera on dist >0.8m
-            if (aRcamera.transform.position.y - yGround > 1.7f) {
-                yGround = aRcamera.transform.position.y - 1.7f;  // assume that a ground couldn't more far than 1.7m
+            yGround = minY;                                 // store closest plane to the camera on dist >MIN
+            if (aRcamera.transform.position.y - yGround > yGroundMax) {
+                yGround = aRcamera.transform.position.y - yGroundMax;  // assume that a ground couldn't more far than MAX
             }
             GetComponent<UIManager>().planeDebug(yGround);  // show the found plane Y-coord
         }
         else
         {
-            yGround = aRcamera.transform.position.y - 1.5f; // there're no found planes, assume the ground is in 1.5m
-            planetimer = 0;                                 // force to recalculate the plane in 2secs
+            yGround = aRcamera.transform.position.y - yGroundDef;   // there're no found planes, assume the ground is in DEF
+            planetimer = 0;                                         // force to recalculate the plane in 2secs
         }
     }
 
@@ -65,6 +72,6 @@ public class PlaneManager : MonoBehaviour
         }
         Debug.Log("NOT READY yGround!!!");
 
-        return aRcamera.transform.position.y - 1.5f;        // assume the ground is in 1.5m
+        return aRcamera.transform.position.y - yGroundDef;     // assume the ground is in DEF
     }
 }
